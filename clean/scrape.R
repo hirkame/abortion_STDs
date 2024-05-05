8b73262c40c95d8649238bc99184c1d7940c0225
hirkame <vjtsx.45afv@gmail.com>
2024-05-05 19:56:41 +0200

    Functionalize scrape and scrape teen-births data

--- clean/scrape.R
+++ clean/scrape.R
@@ -2,14 +2,17 @@ library(RSelenium)
 library(rvest)
 library(readr)
 
+
 # State code
 state_fips_master <- read_csv("data/county_fips_master.csv")
+
 
 # Function to navigate to the given URL and wait for the page to load
 navigate_and_wait <- function(driver, url) {
   driver$navigate(url)
   Sys.sleep(3)
 }
+
 
 # Function to click the 'More' button if it exists
 click_more_button <- function(driver) {
@@ -23,6 +26,7 @@ click_more_button <- function(driver) {
   } 
 }
 
+
 # Function to extract and print the table if it exists
 extract_table <- function(page) {
   if (length(html_nodes(page, "table")) > 0) {
@@ -32,40 +36,57 @@ extract_table <- function(page) {
   }
 }
 
-# Start RSelenium driver
-rD <- rsDriver(browser = "firefox", port = 4555L, verbose = F, chromever = NULL)
-remDr <- rD[["client"]]
 
-# Loop through years and states
-for (state in unique(state_fips_master$state)[!is.na(unique(state_fips_master$state))]) {
-  for (year in 2011:2024) {
-    # Define the URL of the website
-    url <- paste0("https://www.countyhealthrankings.org/health-data/health-factors/health-behaviors/sexual-activity/sexually-transmitted-infections?year=",
-                  year,
-                  "&state=",
-                  sprintf("%02d", state), # Format state code with leading zeros if necessary
-                  "&tab=1")
-    
-    # Navigate to the URL and wait
-    navigate_and_wait(remDr, url)
-    
-    # Click the 'More' button
-    click_more_button(remDr)
-    
-    # Extract the page source
-    page_source <- remDr$getPageSource()[[1]]
-    page <- read_html(page_source)
-    
-    # Extract and print the table
-    table <- extract_table(page)
-    
-    # Save table as csv
-    file_name <- paste0("data/STDs/", state, "_", year, ".csv")
-    write.csv(table, file_name)
-    Sys.sleep(1)
+# Function to scrape data
+scrape <- function (outcome) {
+  
+  # Start RSelenium driver
+  rD <- rsDriver(browser = "firefox", port = 4555L, verbose = F, chromever = NULL)
+  remDr <- rD[["client"]]
+  
+  # Loop through years and states
+  for (state in unique(state_fips_master$state)[!is.na(unique(state_fips_master$state))]) {
+    for (year in 2011:2024) {
+      
+      # Define the URL of the website
+      url <- paste0("https://www.countyhealthrankings.org/health-data/health-factors/health-behaviors/sexual-activity/", 
+                    outcome, 
+                    "?year=",
+                    year,
+                    "&state=",
+                    sprintf("%02d", state), # Format state code with leading zeros if necessary
+                    "&tab=1")
+      
+      # Navigate to the URL and wait
+      navigate_and_wait(remDr, url)
+      
+      # Click the 'More' button
+      click_more_button(remDr)
+      
+      # Extract the page source
+      page_source <- remDr$getPageSource()[[1]]
+      page <- read_html(page_source)
+      
+      # Extract and print the table
+      table <- extract_table(page)
+      
+      # Save table as csv
+      file_name <- paste0("data/", outcome, "/", state, "_", year, ".csv")
+      write.csv(table, file_name)
+      Sys.sleep(0.5)
+      
+    }
   }
+  
+  # Close the Selenium browser
+  remDr$close()
+  rD[["server"]]$stop()
+  
 }
 
-# Close the Selenium browser
-remDr$close()
-rD[["server"]]$stop()
+
+# Run  --------------------------------------------------------------------
+
+scrape("sexually-transmitted-infections")
+scrape("teen-births")
+
