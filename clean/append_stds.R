fcbf9520b81f6f538db92fa02050ce7b42912800
hirkame <vjtsx.45afv@gmail.com>
2024-05-05 19:57:21 +0200

    Renamed var names

--- clean/append_stds.R
+++ clean/append_stds.R
@@ -8,8 +8,8 @@ state_fips_master <- read_csv("data/county_fips_master.csv")
 
 
 # Initialize an empty list to store data frames
-df_std_county <- list()
-df_std_state <- list()
+df_county <- list()
+df_state <- list()
 
 
 # Loop over years and files, excluding specified indices
@@ -17,7 +17,7 @@ for (year in 2011:2024) {
   for (fips in unique(state_fips_master$state)[!is.na(unique(state_fips_master$state))]) {
     # Import data
     df <- read.csv(
-      paste0("data/STDs/", fips, "_", year, ".csv"),
+      paste0("data/sexually-transmitted-infections/", fips, "_", year, ".csv"),
       col.names = c(
         "index",
         "county_name",
@@ -28,8 +28,8 @@ for (year in 2011:2024) {
     )
     
     # County
-    df_county <- df |> 
-      slice(2:n()) |> 
+    county <- df |>
+      slice(2:n()) |>
       select(!index) |>
       mutate(# Generate year and fips
         year = year,
@@ -44,8 +44,8 @@ for (year in 2011:2024) {
       ))
     
     # State
-    df_state <- df |> 
-      slice_head() |> 
+    state <- df |>
+      slice_head() |>
       select(!index) |>
       mutate(# Generate year and fips
         year = year,
@@ -60,48 +60,48 @@ for (year in 2011:2024) {
       ))
     
     # Append data frame to the list
-    df_std_county <- append(df_std_county, list(df_county))
-    df_std_state <- append(df_std_state, list(df_state))
+    df_county <- append(df_county, list(county))
+    df_state <- append(df_state, list(state))
+    
   }
 }
 
-
 # Combine all data frames in the list into a single data frame
-df_std_county <- rbindlist(df_std_county)
-df_std_state <- rbindlist(df_std_state)
-
-
+df_county <- rbindlist(df_county)
+df_state <- rbindlist(df_state)
 # Modify county names
-df_std_county <- df_std_county |> 
+df_county <- df_county |>
   mutate(
-    county_name = str_replace(county_name, "\\^", ""), 
-    county_name = str_replace(county_name, "\\*\\*", ""), 
+    county_name = str_replace(county_name, "\\^", ""),
+    county_name = str_replace(county_name, "\\*\\*", ""),
     county_name = str_replace(county_name, "\\s+$", ""),
     county_name = str_to_title(county_name)
-  ) |> 
+  ) |>
   relocate(year, fips_state, county_name, everything())
-df_std_state <- df_std_state |> 
-  relocate(year, fips_state, county_name, everything()) |> 
+
+# Relocate and arrage state data
+df_state <- df_state |>
+  relocate(year, fips_state, county_name, everything()) |>
   arrange(fips_state, year)
-
-
 
 # Valdez-Cordova Census Area, AK
 # <= Chugach Census Area, AK, Copper River Census Area, AK
-df_std_county <- df_std_county |> 
+df_county <- df_county |>
   bind_rows(
     tibble(
       year = 2024,
       fips_state = 2,
       county_name = "Valdez-Cordova",
-      chlamydiacases = sum(df_std_county[df_std_county$county_name %in% c("Chugach", "Copper River")]$chlamydiacases), 
-      countyvalue = sum(df_std_county[df_std_county$county_name %in% c("Chugach", "Copper River")]$countyvalue),
+      chlamydiacases = sum(df_county[df_county$county_name %in% c("Chugach", "Copper River")]$chlamydiacases),
+      countyvalue = sum(df_county[df_county$county_name %in% c("Chugach", "Copper River")]$countyvalue),
       zscore = NA
     )
-  ) |> 
+  ) |>
   filter(!(county_name %in% c("Chugach", "Copper River"))) 
+  
 
+# Run ---------------------------------------------------------------------
 
 # Save appended dataset
-saveRDS(df_std_county, "data/stds_county.rds")
-saveRDS(df_std_state, "data/stds_state.rds")
+saveRDS(df_county, "data/stds_county.rds")
+saveRDS(df_state, "data/stds_state.rds")
