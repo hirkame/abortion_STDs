5ce63701b8a02d8faa4e9ac64cb08f8e24b840fd
hirkame <vjtsx.45afv@gmail.com>
2024-05-07 10:58:13 +0200

    Updated map code to incorporate births data

--- describe/map.R
+++ describe/map.R
@@ -3,32 +3,32 @@ library(sf)
 
 
 # Read data
-df_std_state <- read_rds("data/stds_state.rds")
-df_std_county <- read_rds("data/stds_county.rds")
+df_state <- read_rds("data/state_data.rds")
+df_county <- read_rds("data/county_data.rds")
 states <- st_read("data/map/cb_2018_us_state_500k/cb_2018_us_state_500k.shp")
 county <- st_read("data/map/cb_2018_us_county_500k/cb_2018_us_county_500k.shp")
 
 
 # Calculate growth rates of chlamydia cases
-df_std_state <- df_std_state |> 
+df_state <- df_state |> 
   mutate(
     fips_state = sprintf("%02d", fips_state)
   ) |> 
   mutate(
-    growth_rate = (countyvalue - lag(countyvalue))/lag(countyvalue)*100,
+    growth_rate_chlamydiacases = (chlamydiacases_rate - lag(chlamydiacases_rate))/lag(chlamydiacases_rate)*100,
+    growth_rate_teenbirths = (teenbirth_rate - lag(teenbirth_rate))/lag(teenbirth_rate)*100,
     .by = "fips_state"
-  ) |> 
-  filter(!is.na(growth_rate))
+  ) 
 
-df_std_county <- df_std_county |> 
+df_county <- df_county |> 
   mutate(
     fips = sprintf("%05d", fips)
   ) |> 
   mutate(
-    growth_rate = (countyvalue - lag(countyvalue))/lag(countyvalue)*100,
-    .by = "fips"
-  ) |> 
-  filter(!is.na(growth_rate))
+    growth_rate_chlamydiacases = (chlamydiacases_rate - lag(chlamydiacases_rate))/lag(chlamydiacases_rate)*100,
+    growth_rate_teenbirths = (teenbirth_rate - lag(teenbirth_rate))/lag(teenbirth_rate)*100,
+    .by = c("fips_state", "county_name")
+  ) 
 
 
 # Remove Alaska and Hawaii
@@ -40,9 +40,9 @@ county <- county |>
 
 # Merge data
 states <- states |> 
-  left_join(df_std_state, by = join_by(STATEFP == fips_state))
+  left_join(df_state, by = join_by(STATEFP == fips_state))
 county <- county |> 
-  left_join(df_std_county, by = join_by(GEOID == fips))
+  left_join(df_county, by = join_by(GEOID == fips))
 
 
 
@@ -51,8 +51,8 @@ states <- states |>
   filter(year %in% 2021:2024) 
 
 ggplot() + 
-  geom_sf(data = states, aes(fill = growth_rate), lty = 0) +
-  scale_fill_gradient(
+  geom_sf(data = states, aes(fill = growth_rate_chlamydiacases), lty = 0) +
+  scale_fill_gradient2(
     high = "red",
     mid = "white",
     low = "blue"
@@ -66,22 +66,17 @@ ggplot() +
     plot.title = element_text(size=12)
   )
 
-ggsave("describe/state_growth.png", width = 20, height = 12, units = "cm")
+ggsave("describe/state_growth_chlamydia.png", width = 20, height = 12, units = "cm")
 
-
-# County-level  -----------------------------------------------------------
-county <- county |> 
-  filter(year %in% 2021:2024)
-  
 ggplot() + 
-  geom_sf(data = county, aes(fill = growth_rate), lty = 0) +
+  geom_sf(data = states, aes(fill = growth_rate_teenbirths), lty = 0) +
   scale_fill_gradient2(
     high = "red",
     mid = "white",
     low = "blue"
   ) + 
-  labs(title = "Growth of new chlamydia cases per 100,000 people (County, %)") +
-  facet_wrap(~ year)  + 
+  facet_wrap(~ year) +
+  labs(title = "Growth of teen births per 1,000 people (State, %)") + 
   theme(
     text = element_text(size = 8), 
     strip.text = element_text(face="bold"),
@@ -89,7 +84,48 @@ ggplot() +
     plot.title = element_text(size=12)
   )
 
-ggsave("describe/county_growth.png", width = 20, height = 12, units = "cm")
+ggsave("describe/state_growth_teenbirths.png", width = 20, height = 12, units = "cm")
 
+
+
+# County-level  -----------------------------------------------------------
+county <- county |> 
+  filter(year %in% 2021:2024)
   
+ggplot() + 
+  geom_sf(data = county, aes(fill = growth_rate_chlamydiacases), lty = 0) +
+  scale_fill_gradient2(
+    high = "red",
+    mid = "white",
+    low = "blue"
+  ) + 
+  labs(title = "Growth of new chlamydia cases per 100,000 people (County, %)") +
+  facet_wrap(. ~ year)  + 
+  theme(
+    text = element_text(size = 8), 
+    strip.text = element_text(face="bold"),
+    strip.background = element_blank(),
+    plot.title = element_text(size=12)
+  )
+
+ggsave("describe/county_growth_chlamydia.png", width = 20, height = 12, units = "cm")
+
+
+ggplot() + 
+  geom_sf(data = county, aes(fill = growth_rate_teenbirths), lty = 0) +
+  scale_fill_gradient2(
+    high = "red",
+    mid = "white",
+    low = "blue"
+  ) + 
+  labs(title = "Growth of teen births per 1,000 people (County, %)") +
+  facet_wrap(. ~ year)  + 
+  theme(
+    text = element_text(size = 8), 
+    strip.text = element_text(face="bold"),
+    strip.background = element_blank(),
+    plot.title = element_text(size=12)
+  )
+
+ggsave("describe/county_growth_teenbirths.png", width = 20, height = 12, units = "cm")
 
